85b6604df9e02276b66a01336ce3d292bbb4de61
Shinichi99 <s.moribe0909@gmail.com>
2023-10-22 17:31:23 +0200

    change setting file

--- airbnb_tokyo/settings.py
+++ airbnb_tokyo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m+c17^!nc1*8^e0-5i9jcu^j9%8pz8x$s_(6c*-45(@v&75f_0"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -81,6 +81,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+DATABASES["default"] = dj_database_url.parse("postgres://airbnb_tokyo_django_user:qGrSxkmuD4sJnd9rJabC5LoPE2Bd3KxH@dpg-ckqjpsg5vl2c738162p0-a.oregon-postgres.render.com/airbnb_tokyo_django")
 
 
 # Password validation
