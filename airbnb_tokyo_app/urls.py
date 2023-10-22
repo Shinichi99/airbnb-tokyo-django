1d3000b48842afd13430d6bde0406fdc1c57c402
Shinichi99 <s.moribe0909@gmail.com>
2023-10-22 23:16:08 +0200

    update urls.py files

--- airbnb_tokyo_app/urls.py
+++ airbnb_tokyo_app/urls.py
@@ -1,7 +1,12 @@
 from . import views
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('result/', views.result, name='result'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
